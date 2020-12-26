152b82a7fe7b57c1a47afde45adee29303d591eb
Your Name <you@example.com>
2020-12-27 00:22:57 +0100

    innitial commit

--- myApp.js
+++ myApp.js
@@ -2,13 +2,20 @@ var express = require('express');
 var app = express();
 
 
-app.get('/name', (req, res) => {
-    res.json({"hello": "muhammad"})
-})
+// app.get('/name', (req, res) => {
+//     res.json({"hello": "muhammad"})
+// })
 
 
-
-
+app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
+  
 
 
 
