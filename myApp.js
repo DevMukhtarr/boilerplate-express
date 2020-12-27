074c4ae0df069f0e4fbc1fbe26910e68f80c4ceb
Your Name <you@example.com>
2020-12-27 23:18:14 +0100

    db

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-require('dotenv').config()
 
 
 // app.get('/name', (req, res) => {
